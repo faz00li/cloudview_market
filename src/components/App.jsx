d08720cc6ddfd7a61c8f3017a20ef2f29c424c5b
Eddie Raskin <eddie.raskin@gmail.com>
2020-02-26 20:10:01 -0800

    update routes and imports to App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,9 @@ import { Switch, Route } from 'react-router-dom';
 import Header from './Header';
 import Main from './Main';
 import Error404 from './Error404';
+import WeeklySchedule from './WeeklySchedule';
+import SeasonalCrops from './SeasonalCrops';
+
 
 
 function App(){
@@ -12,6 +15,8 @@ function App(){
       
       <Switch>
         <Route exact path='/' component={Main} />
+        <Route path="/schedule" component={WeeklySchedule} />
+        <Route path="/seasons" component={SeasonalCrops} />
         <Route component={Error404} />
        
       </Switch>

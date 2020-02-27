9b8a74e0fc5935df8a962ec7ad1981c049e046af
Eddie Raskin <eddie.raskin@gmail.com>
2020-02-26 20:24:23 -0800

    update App w/ routes and imports

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,23 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Error404 from './Error404';
 import Header from './Header';
 import Main from './Main';
-import Error404 from './Error404';
 import WeeklySchedule from './WeeklySchedule';
 import SeasonalCrops from './SeasonalCrops';
-
-
+import AboutUs from './AboutUs';
 
 function App(){
   return (
-    <div className="container-fluid">
+    <div className='container-fluid'>
       <Header/>
       
       <Switch>
         <Route exact path='/' component={Main} />
-        <Route path="/schedule" component={WeeklySchedule} />
-        <Route path="/seasons" component={SeasonalCrops} />
+        <Route path='/schedule' component={WeeklySchedule} />
+        <Route path='/seasons' component={SeasonalCrops} />
+        <Route path='/about' component={AboutUs} />
         <Route component={Error404} />
-       
       </Switch>
     </div>
   );
--- src/components/WeeklySchedule.jsx
+++ src/components/WeeklySchedule.jsx
@@ -5,16 +5,11 @@ function WeeklySchedule(props){
   return (
     <div>
       <h1>WeeklySchedule</h1>
-      {/* <h3>{props.day}</h3>
-      <p><em>{props.location}</em></p> */}
+      {/* will need map */}
       <hr/>
     </div>
   );
 }
 
-// MarketDay.propTypes = {
-//   day: PropTypes.string.isRequired,
-//   location: PropTypes.string.isRequired
-// };
 
 export default WeeklySchedule;
